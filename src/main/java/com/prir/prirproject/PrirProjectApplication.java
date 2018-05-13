b4beb5e804d106cb48d18a850c4cc3fd6a7b2c28
Filip <filip.janusz57@gmail.com>
2018-05-13 12:04:37 +0200

    Initial commit

---
+++ src/main/java/com/prir/prirproject/PrirProjectApplication.java
@@ -0,0 +1,12 @@
+package com.prir.prirproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrirProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrirProjectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/prir/prirproject/PrirProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prir.prirproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrirProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
